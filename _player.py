2a297c087e62c4f9cd5a79085b985b8f46daf484
Trey Hamilton <32377378+treyham91@users.noreply.github.com>
2018-04-02 23:53:23 -0500

    Add files via upload

---
+++ _player.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  2 12:15:22 2018
+
+@author: treyh
+"""
+
+class Player:
+    
+    def __init__(self, name, age, owgr, fedex_rank, years_pro):
+        self.name = name
+        self.age = age
+        self.owgr = owgr
+        self.fedex_rank = fedex_rank
+        self.years_pro = years_pro
+        
+    def playerNameAndRank(self):
+        print('{} is currently ranked {} in the world, and {} in the Fedex Cup Standings'
+              .format(self.name, self.owgr, self.fedex_rank))
+        
+    def ageTurnedPro(self):
+        self.age_turned_pro = self.age - self.years_pro
+        print('{} was {} years old when he turned pro'.format(self.name, self.age_turned_pro))
+        
+    def avgDrive(self):
+        self.avg_drive = self.drive_total / self.number_of_drives
+        print('{} average drive this year is {}'.format(self.name, self.avg_drive))
+
+
+player = Player('Rickie Fowler', 29, 8, 15, 9)
+player.playerNameAndRank()
+player.ageTurnedPro()
+        
+
+        
+    
