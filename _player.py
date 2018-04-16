9de4c7aa894a11f93d69b55c3afd86d4fde0e0af
Trey Hamilton <32377378+treyham91@users.noreply.github.com>
2018-04-15 23:09:28 -0500

    Update _player.py

--- _player.py
+++ _player.py
@@ -14,19 +14,19 @@ class Player:
         self.fedex_rank = fedex_rank
         self.years_pro = years_pro
         
-    def playerNameAndRank(self):
+    def player_name_and_rank(self):
         print('{} is currently ranked {} in the world, and {} in the Fedex Cup Standings'
               .format(self.name, self.owgr, self.fedex_rank))
         
-    def ageTurnedPro(self):
+    def age_turned_pro(self):
         self.age_turned_pro = self.age - self.years_pro
         print('{} was {} years old when he turned pro'.format(self.name, self.age_turned_pro))
         
-    def avgDrive(self):
+    def avg_drive(self):
         self.avg_drive = self.drive_total / self.number_of_drives
         print('{} average drive this year is {}'.format(self.name, self.avg_drive))
         
-    def plotPlayerRanking(self):
+    def plot_player_ranking(self):
         x = self.owgr
         y = self.years_pro
         fig, ax = plt.subplots()
@@ -36,9 +36,10 @@ class Player:
 def main():
     
     player = Player('Rickie Fowler', 29, 8, 15, 9)
-    player.playerNameAndRank()
-    player.ageTurnedPro()
-    player.plotPlayerRanking()
+    player.player_name_and_rank()
+    player.age_turned_pro()
+    player.avg_drive()
+    player.plot_player_ranking()
     
 if __name__ == "__main__":
     main()
